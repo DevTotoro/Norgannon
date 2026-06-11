e5fe442165ffd2c0f92d85ba65c2f581e6467d66
DevTotoro <totoro.softwaredev@gmail.com>
2026-06-12 00:30:37 +0100

    Add User entity tests

--- packages/api/src/main/kotlin/org/norgannon/api/ApiApplication.kt
+++ packages/api/src/main/kotlin/org/norgannon/api/ApiApplication.kt
@@ -2,10 +2,8 @@ package org.norgannon.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class ApiApplication
 
 fun main(args: Array<String>) {
---
+++ packages/api/src/main/kotlin/org/norgannon/api/infrastructure/config/JpaAuditingConfig.kt
@@ -0,0 +1,15 @@
+package org.norgannon.api.infrastructure.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.auditing.DateTimeProvider
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.time.OffsetDateTime
+import java.util.Optional
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "offsetDateTimeProvider")
+class JpaAuditingConfig {
+    @Bean
+    fun offsetDateTimeProvider(): DateTimeProvider = DateTimeProvider { Optional.of(OffsetDateTime.now()) }
+}
