6fb3816271560756055028ea2cc870ea69eee01f
DevTotoro <totoro.softwaredev@gmail.com>
2026-06-13 03:30:35 +0100

    Create dev user for local development

--- infra/keycloak/entrypoint.sh
+++ infra/keycloak/entrypoint.sh
@@ -2,8 +2,10 @@
 set -euo pipefail
 
 : "${KEYCLOAK_REALM_NAME:?KEYCLOAK_REALM_NAME must be set}"
+: "${DEV_USER_EMAIL:?DEV_USER_EMAIL must be set}"
+: "${DEV_USER_PASSWORD:?DEV_USER_PASSWORD must be set}"
 
-TEMPLATE_FILE="/opt/keycloak/data/import-template/thevideoclub.realm.json.tpl"
+TEMPLATE_FILE="/opt/keycloak/data/import-template/thevideoclub.realm.json"
 IMPORT_DIR="/opt/keycloak/data/import"
 OUTPUT_FILE="$IMPORT_DIR/thevideoclub.realm.json"
 
@@ -14,10 +16,20 @@ fi
 
 mkdir -p "$IMPORT_DIR"
 
+escape_for_sed() {
+  printf '%s' "$1" | sed -e 's/[&\/|]/\\&/g'
+}
 
-realm_escaped=$(printf '%s' "$KEYCLOAK_REALM_NAME" | sed 's/[&\/|]/\\&/g')
-sed "s|__KEYCLOAK_REALM_NAME__|$realm_escaped|g" "$TEMPLATE_FILE" > "$OUTPUT_FILE"
-echo "Generated realm config at $OUTPUT_FILE with KEYCLOAK_REALM_NAME=${KEYCLOAK_REALM_NAME}"
+escaped_realm=$(escape_for_sed "$KEYCLOAK_REALM_NAME")
+escaped_email=$(escape_for_sed "$DEV_USER_EMAIL")
+escaped_password=$(escape_for_sed "$DEV_USER_PASSWORD")
+
+sed -e "s|__KEYCLOAK_REALM_NAME__|$escaped_realm|g" \
+    -e "s|__DEV_USER_EMAIL__|$escaped_email|g" \
+    -e "s|__DEV_USER_PASSWORD__|$escaped_password|g" \
+    "$TEMPLATE_FILE" > "$OUTPUT_FILE"
+
+echo "Generated realm config at $OUTPUT_FILE"
 
 # Start Keycloak
 exec /opt/keycloak/bin/kc.sh start-dev --import-realm
